5c560d19dca7c8de98a87a9be177345d859e69aa
Jamie Tsao <jamietsao@gmail.com>
2017-04-04 15:08:15 -0700

    zsh changes

--- zsh/env.sh
+++ zsh/env.sh
@@ -7,15 +7,24 @@ export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 # go
 export GOPATH=/Users/jamie/workspace/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+alias gosrc='cd $GOPATH/src/github.com/gametimesf'
 
 # maven
 export PATH=/Users/jamie/workspace/lib/apache-maven-3.3.3/bin:$PATH
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+. "/usr/local/opt/nvm/nvm.sh"
 
 # java
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
 
 # ruby
 export BUNDLER_EDITOR=subl
+eval "$(rbenv init -)"
+
+# python
+eval "$(pyenv init -)"
 
 # Gametime scripts
 export PATH=$PATH:~/workspace/gametime/bin
@@ -45,6 +54,3 @@ export PATH="/usr/local/heroku/bin:$PATH"
 
 # awscli
 source /usr/local/share/zsh/site-functions/_aws
-
-# rbenv
-eval "$(rbenv init -)"
